fe0aa79bc87c40b6705c11a335251c1038e31c12
David gh <jh0369549@gmail.com>
2024-11-20 13:57:59 +0330

    setup models (post and user)

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
 # Create your models here .
+class User(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(max_length=254)
+    birth_date = models.DateField(null = Ture , blank = True)
+    password = modesl.CharField(max_length=50)
 
 class Post(models.Model):
+    title = models.CharField(max_length=250)
+    context = models.TextField()
+    cteated_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField( auto_now_add=True)
+    is_published = models.BooleanField(default = False)
+    published_date = models.DateTimeField(auto_now_add=True)
+    auther = models.ForeignKey(User ,on_delete=models.CASCADE)
